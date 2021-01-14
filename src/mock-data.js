dbf7b54ce766928dd91808d4be659bebea5999ca
Clemfrl <63780455+Clemfrl@users.noreply.github.com>
2021-01-14 16:30:13 +0100

    Task 4.3 / stuck in Suggestions list creation

---
+++ src/mock-data.js
@@ -0,0 +1,64 @@
+const mockData = [
+  {
+    kind: "calendar#event",
+    etag: '"3220717754038000"',
+    id: "4rdncc7p5sjfhqoma6775naasr",
+    status: "confirmed",
+    htmlLink:
+      "https://www.google.com/calendar/event?eid=NHJkbmNjN3A1c2pmaHFvbWE2Nzc1bmFhc3IgY2xlbWVudC5mcmhsY2hAbQ",
+    created: "2021-01-11T09:54:37.000Z",
+    updated: "2021-01-11T09:54:37.019Z",
+    summary: "google suxxx",
+    creator: {
+      email: "clement.frhlch@gmail.com",
+      self: true,
+    },
+    organizer: {
+      email: "clement.frhlch@gmail.com",
+      self: true,
+    },
+    start: {
+      dateTime: "2021-01-11T13:00:00+01:00",
+    },
+    end: {
+      dateTime: "2021-01-11T14:00:00+01:00",
+    },
+    iCalUID: "4rdncc7p5sjfhqoma6775naasr@google.com",
+    sequence: 0,
+    reminders: {
+      useDefault: true,
+    },
+  },
+  {
+    kind: "calendar#event",
+    etag: '"3220717754038000"',
+    id: "4rdncc7p5sjfhqoma6775naasr",
+    status: "confirmed",
+    htmlLink:
+      "https://www.google.com/calendar/event?eid=NHJkbmNjN3A1c2pmaHFvbWE2Nzc1bmFhc3IgY2xlbWVudC5mcmhsY2hAbQ",
+    created: "2021-01-11T09:54:37.000Z",
+    updated: "2021-01-11T09:54:37.019Z",
+    summary: "google suxxx",
+    creator: {
+      email: "clement.frhlch@gmail.com",
+      self: true,
+    },
+    organizer: {
+      email: "clement.frhlch@gmail.com",
+      self: true,
+    },
+    start: {
+      dateTime: "2021-01-11T13:00:00+01:00",
+    },
+    end: {
+      dateTime: "2021-01-11T14:00:00+01:00",
+    },
+    iCalUID: "4rdncc7p5sjfhqoma6775naasr@google.com",
+    sequence: 0,
+    reminders: {
+      useDefault: true,
+    },
+  },
+];
+
+export { mockData };
