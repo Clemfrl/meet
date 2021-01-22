174eef4c2ebfce89dd43ee7db5dcb5df2c85a851
Clemfrl <63780455+Clemfrl@users.noreply.github.com>
2021-01-22 13:23:09 +0100

    4.4 : not the same error than the course

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,30 +1,40 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from '../App';
-import EventList from '../EventList';
-import CitySearch from '../CitySearch';
-import Event from '../Event';
-import NumberOfEvents from '../NumberOfEvents';
+import React from "react";
+import App from "../App";
+import EventList from "../EventList";
+import CitySearch from "../CitySearch";
+import Event from "../Event";
+import NumberOfEvents from "../NumberOfEvents";
+import { shallow, mount } from "enzyme";
 
-describe('<App /> component', () => {
+describe("<App /> component", () => {
   let AppWrapper;
   beforeAll(() => {
     AppWrapper = shallow(<App />);
-  })
+  });
 
-  test('render list of events', () => {
+  test("render list of events", () => {
     expect(AppWrapper.find(EventList)).toHaveLength(1);
   });
 
-  test('render CitySearch', () => {
+  test("render CitySearch", () => {
     expect(AppWrapper.find(CitySearch)).toHaveLength(1);
   });
 
-  test('render event', () => {
+  test("render event", () => {
     expect(AppWrapper.find(Event)).toHaveLength(1);
   });
 
-  test('render NumberOfEvents', () => {
+  test("render NumberOfEvents", () => {
     expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
-  })
+  });
+});
+
+describe("<App /> integration", () => {
+  test('App passes "events" state as a prop to EventList', () => {
+    const AppWrapper = mount(<App />);
+    const AppEventsState = AppWrapper.state("events");
+    expect(AppEventsState).not.toEqual(undefined);
+    expect(AppWrapper.find(EventList).props().events).toEqual(AppEventsState);
+    AppWrapper.unmount();
+  });
 });
