6768b897bb8e855f27306520367b37080ef29cd0
Clemfrl <63780455+Clemfrl@users.noreply.github.com>
2021-02-04 11:00:02 +0100

    Update test condition

--- src/api.js
+++ src/api.js
@@ -52,7 +52,7 @@ const getToken = async (code) => {
 export const getEvents = async () => {
   NProgress.start();
 
-  if (window.location.href.startsWith("http://localhost")) {
+  if (window.location.href.endsWith("?mock")) {
     NProgress.done();
     return mockData;
   }
