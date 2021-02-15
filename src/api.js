1245616c1bfce7828963242d6f7469c4e7b6df8b
Clemfrl <63780455+Clemfrl@users.noreply.github.com>
2021-02-15 09:56:07 +0100

    rewrite again serverless url

--- src/api.js
+++ src/api.js
@@ -35,7 +35,9 @@ const removeQuery = () => {
 const getToken = async (code) => {
   const encodeCode = encodeURIComponent(code);
   const { access_token } = await fetch(
-    `https://kp1nuhgxag.execute-api.us-east-1.amazonaws.com/dev/api/token${encodeCode}`
+    "https://kp1nuhgxag.execute-api.us-east-1.amazonaws.com/dev/api/token" +
+      "/" +
+      encodeCode
   )
     .then((res) => {
       return res.json();
@@ -55,24 +57,26 @@ export const getEvents = async () => {
     return mockData;
   }
 
-  if (
-    !navigator.onLine &&
-    !window.location.href.startsWith("http://localhost")
-  ) {
+  if (!navigator.onLine) {
     const events = localStorage.getItem("lastEvents");
     NProgress.done();
-    //return { events: JSON.parse(events).events, locations: extractLocations(JSON.parse(events).events) };
-    return JSON.parse(events).events;
+    return {
+      events: JSON.parse(events).events,
+      locations: extractLocations(JSON.parse(events).events),
+    };
   }
 
   const token = await getAccessToken();
 
   if (token) {
     removeQuery();
-    const url = `https://kp1nuhgxag.execute-api.us-east-1.amazonaws.com/dev/api/get-events/${token}`;
+    const url =
+      `https://kp1nuhgxag.execute-api.us-east-1.amazonaws.com/dev/api/get-events` +
+      "/" +
+      token;
     const result = await axios.get(url);
     if (result.data) {
-      let locations = extractLocations(result.data.events);
+      var locations = extractLocations(result.data.events);
       localStorage.setItem("lastEvents", JSON.stringify(result.data));
       localStorage.setItem("locations", JSON.stringify(locations));
     }
